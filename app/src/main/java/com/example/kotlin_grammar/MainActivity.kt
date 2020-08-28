4190ae28143786fe083f954305d496e13c261330
2taezeat <59787852+2taezeat@users.noreply.github.com>
2020-08-28 22:33:08 +0900

    200928 / Kotlin_Grammar / ch3-6_function

--- app/src/main/java/com/example/kotlin_grammar/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_grammar/MainActivity.kt
@@ -49,7 +49,7 @@ class MainActivity : AppCompatActivity() {
         Log.d("BasicSyntax","두 번째 값은 ${slist.get(1)}.")
         Log.d("BasicSyntax","세 번째 값은 ${slist.get(2)}.")
         Log.d("BasicSyntax","slist에는 ${slist.size}개의 값이 있습니다.")
-        */
+        *//*
         // 4-1 기본 타입 배열 선언하기
         var students = IntArray(10)
         // 4-2 문자열 타입 배열 선언하기
@@ -66,12 +66,85 @@ class MainActivity : AppCompatActivity() {
         Log.d("BasicSyntax","세 번째 값은 ${a}.")
         Log.d("BasicSyntax","세 번째 값은 ${students[8]}.")
         Log.d("BasicSyntax","세 번째 값은 ${students[9]}.")
+        *//*
+        // 4-1 맵 생성하기
+        var map1 = mutableMapOf<String, String>()
 
+        map1.put("key1", "value1")
+        map1.put("key2", "value2")
+        map1.put("key3", "value3")
+        var v = map1.get("key2")
+        Log.d("BasicSyntax","key2 값은 ${v}.")
+        map1.put("key2","dskjfhskdjfh")
+        Log.d("BasicSyntax","key2 값은 ${map1.get("key2")}.")
+        map1.remove("key2")
+        Log.d("BasicSyntax","key2 값은 ${map1.get("key2")}.")
+        */
+        // 5장 반복문
+        /* for ( i in 1..10)
+        {
+            Log.d("BasicSyntax","i의 값은 ${i}.")
+        }
+        var array1 = arrayOf("asd","tery","xcv")
+        for ( i in 0 until array1.size)
+        {
+            Log.d("BasicSyntax","i의 값은 ${i}.")
+        }
+        for ( i in 0..10 step 4)
+        {
+            Log.d("BasicSyntax","i의 값은 ${i}.")
+        }
+        for ( i in 10 downTo 0)
+        {
+            Log.d("BasicSyntax","i의 값은 ${i}.")
+        }
+        for ( i in 10 downTo 0 step 3)
+        {
+            Log.d("BasicSyntax","i의 값은 ${i}.")
+        }
+        */
+        /*
+        for ( e in 1..10)
+        {
+            if (e > 3 && e < 8 )
+            {
+                continue
+            }
+            Log.d("BasicSyntax","e의 값은 ${e}.")
+        }
+        */
+        /*
+        var sr = square(30)
+        Log.d("BasicSyntax","sr의 값은 ${sr}.")
 
+        printsum(3,6)
+        val PI = getpi()
+        Log.d("BasicSyntax","pi의 값은 ${PI}.")
+
+        newfunction1("hello")
+        newfunction1("micadsas", weight = 456.12)
+        */
         /**/
-        /**/
-        /**/
-        /**/
+
 
     }
+}
+///////////////////////////////////////////////////////////////////////////////////
+
+fun square(x: Int ): Int {
+    return (x*x)
+}
+
+fun printsum(x:Int, y:Int){
+    Log.d("BasicS5yntax","x + y = ${x+y}.")
+}
+fun getpi(): Double {
+    return 3.14
+}
+
+fun newfunction1(name:String, age: Int = 29, weight:Double = 65.6)
+{
+    Log.d("BasicS5yntax","name = ${name}.")
+    Log.d("BasicS5yntax","age = ${age}.")
+    Log.d("BasicS5yntax","weight = ${weight}.")
 }
